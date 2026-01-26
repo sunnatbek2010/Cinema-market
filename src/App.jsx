138586f6f8270b213ae6972a9f615b319849ed6c
farruhabdurahmanov669-code <farruhabdurahmanov669@gmail.com>
2026-01-26 15:52:27 +0500

    marketplace

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
+import Marketplace from "./pages/Marketplace";
 
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="marketplace" element={<Marketplace />} />
+      </Route>
+    </Routes>
   )
 }
+
 
 export default App
